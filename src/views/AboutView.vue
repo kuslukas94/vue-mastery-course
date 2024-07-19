23d6dc2880ce491833c4edc75c4b632dd2309236
Lukáš Kus <kus.lukas@seznam.cz>
2024-07-19 09:34:51 +0200

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>A site for neighbours to be part of our community.</h1>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/views/EventListView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+import { ref } from 'vue'
+
+const events = ref([
+  
+])
+</script>
+
+<template>
+  <h1>Upcoming Events</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
