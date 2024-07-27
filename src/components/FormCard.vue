de476766286a6ac293ea2bc1d10db854452cd447
DavidKoteles <koteles.d25@gmail.com>
2024-07-27 10:20:12 +0200

    refactor: add comments.

--- src/components/FormCard.vue
+++ src/components/FormCard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue' // TODO: use reactive instead of ref
 import ProductionService from '@/services/ProductionService.js'
 import myUUID from '../stores/UUID'
 
